20a5caa4b19c436564598aece50884591d444060
Anthony Gelder <anthony.gelder@gmail.com>
2020-09-10 21:08:47 -0700

    maxSUmArray

--- algorithms.js
+++ algorithms.js
@@ -187,4 +187,39 @@ function paritySort(A) {
     return newArr
 }
 
-console.log(paritySort([4,2,5,7]))
+// console.log(paritySort([4,2,5,7]))
+
+// -------------------------------------------------------------------------------------------------------------
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+
+function maxSubArray(nums) {
+    // max 
+    let trueMax = 0
+    // loop
+    for(let i = 0; i < nums.length; i++) {
+        let max = nums[i]
+        // inner loop
+        for(let j = i+1; j < nums.length; j++) {
+            max = max + nums[j]
+            // add to temp max and check if it is bigger then max
+            if(max > trueMax) {
+                trueMax = max
+            }
+        }
+    }
+    return trueMax
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
