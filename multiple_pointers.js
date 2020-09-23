dae1613afe5c928ce5dfeb81f4008c93307474ac
Anthony Gelder <anthony.gelder@gmail.com>
2020-09-22 22:19:20 -0700

    multiple pointer and sliding window

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -17,4 +17,41 @@ function zeroSum(arr) {
     }
 }
 
-console.log(zeroSum([-5,-2,-1,0,1,3,4]))
+// console.log(zeroSum([-5,-2,-1,0,1,3,4]))
+
+
+// implement a function called countUnigueValues, which acceptes a sorted array, and counts the unique values in the array. 
+// There can be negative numbers in the array, but it wil always be sorted. 
+
+function countUniqueValues(arr) {
+
+    if(arr.length === 0) {
+        return 0
+    }
+    let i = 0
+
+
+    // start loop with 2 pointers at the start of the array
+    for(let j = 1; j < arr.length; j++ ) {
+        // console.log(i)
+        // check to see if the 2 pointers are equal
+        if(arr[i] !== arr[j]) {
+            // if they are, interate the second pointer
+        
+            // if they are not, iterate 1st pointer and set it equal to the second pointer
+            
+            i++
+            
+            arr[i] = arr[j]
+        }
+        
+        // console.log(arr)
+    }
+    
+    // when second pointer reaches array end, return index of 1st pointer plus 1
+    
+    return i + 1
+
+}
+
+console.log(countUniqueValues([1,1,1,2,3,3,4,4,5,6]))
